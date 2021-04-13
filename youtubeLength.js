5465c2d7d7dfc8f128d7fef9ba7bcec2e34ddd53
SpellBound <junlee36520620@gmail.com>
2021-04-13 21:36:05 +0900

    Add timeConverter return alert() & totalTime return total

--- youtubeLength.js
+++ youtubeLength.js
@@ -1,27 +1,27 @@
 // Number(document.querySelectorAll('.style-scope ytd-thumbnail-overlay-time-status-renderer')[1].children[1].innerText.split(':')[0] * 60)
 // + Number(document.querySelectorAll('.style-scope ytd-thumbnail-overlay-time-status-renderer')[1].children[1].innerText.split(':')[1]);
 
-let text = document.querySelectorAll('.style-scope ytd-thumbnail-overlay-time-status-renderer');
-let total = 0;
+const text = document.querySelectorAll('.style-scope ytd-thumbnail-overlay-time-status-renderer');
+
 // 총 시간 값 구하기
 function totalTime(text) {
+    let total = 0;
     for (let i = 0; i < text.length; i++) {
         // 총 시간 합을 초단위로 환산
         // Number를 사용하여 String형 문자를 정수형으로 변환 후 더하기
-        total += Number(text[i].children[1].innerText.split(':')[0] * 60) + Number(text[i].children[1].innerText.split(':')[1]) ;
+       total += Number(text[i].children[1].innerText.split(':')[0] * 60) + Number(text[i].children[1].innerText.split(':')[1]) ;
     }
+    return total;
 }
 // 시간 변환
 function timeConverter(total) {
-    console.log(total);
+    // console.log(total);
     let h = Math.floor(total / 3600);
-
     let m = Math.floor(total % 3600 / 60);
-
     let s = Math.floor(total % 3600 % 60);
 
     let hScreen = h + "시간, ";
     let mScreen = m + "분, ";
     let sScreen = s + "초";
-    return hScreen + mScreen + sScreen;
+    return alert(hScreen + mScreen + sScreen);
 }
