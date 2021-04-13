8b955f373cc2ac3a3b767285cbe5775ce0bd0f99
SpellBound <junlee36520620@gmail.com>
2021-04-13 21:11:00 +0900

    Add comment

--- youtubeLength.js
+++ youtubeLength.js
@@ -3,13 +3,15 @@
 
 let text = document.querySelectorAll('.style-scope ytd-thumbnail-overlay-time-status-renderer');
 let total = 0;
-
+// 총 시간 값 구하기
 function totalTime(text) {
     for (let i = 0; i < text.length; i++) {
+        // 총 시간 합을 초단위로 환산
+        // Number를 사용하여 String형 문자를 정수형으로 변환 후 더하기
         total += Number(text[i].children[1].innerText.split(':')[0] * 60) + Number(text[i].children[1].innerText.split(':')[1]) ;
     }
 }
-
+// 시간 변환
 function timeConverter(total) {
     console.log(total);
     let h = Math.floor(total / 3600);
